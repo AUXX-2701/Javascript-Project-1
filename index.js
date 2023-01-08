207898cc51ed70851e3f31111abb9eec26de4436
AUXX2701 <73037866+AUXX2701@users.noreply.github.com>
2023-01-08 14:33:44 +0100

    just an update ig git push

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-// const main = document.querySelector('body');
-function createH2(colour, text){
-    const heading2 = "hello";
+const main = document.querySelector('center');
+const joursDeLaSemaine = ["LUNDI", "MARDI", "MERCREDI", "JEUDI", "VENDREDI"];
+
+function createH2(text){
+    const heading2 = document.createElement('h1');
+    heading2.setAttribute('style', 'color: red');
+    heading2.textContent = text;
+    return heading2;
 }
 
 function weekday(){
-    let inputText = document.getElementById("searchBar").value;
-    const joursDeLaSemaine = ["LUNDI", "MARDI", "MERCREDI", "JEUDI", "VENDREDI"];
+    let inputText = document.getElementById("searchBar").value.toUpperCase();
     for(let i = 0; i < (joursDeLaSemaine.length); i++) {
-        if (inputText.toUpperCase() == joursDeLaSemaine[i]){
-            console.log(inputText);
+        if (inputText == joursDeLaSemaine[i]){
+            // console.log(inputText);
             document.getElementById("searchBar").value ='';
+            const edt = createH2('hello');
+            main.appendChild(edt);
             return;
         }
     }
