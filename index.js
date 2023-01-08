d097dbfc68c90883fa15eaec6636e1173cf8e707
AUXX2701 <73037866+AUXX2701@users.noreply.github.com>
2023-01-08 14:07:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // const main = document.querySelector('body');
+function createH2(colour, text){
+    const heading2 = "hello";
+}
 
 function weekday(){
     let inputText = document.getElementById("searchBar").value;
@@ -6,7 +9,10 @@ function weekday(){
     for(let i = 0; i < (joursDeLaSemaine.length); i++) {
         if (inputText.toUpperCase() == joursDeLaSemaine[i]){
             console.log(inputText);
-            break;
+            document.getElementById("searchBar").value ='';
+            return;
         }
     }
+    document.getElementById("searchBar").value ='';
+    alert("Veuillez entrer un jour de la semaine")
 }
